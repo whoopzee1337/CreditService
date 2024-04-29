c8809ffc308879432033a4c4ac8fc57ced0e24e1
Alexandr Malikov <whoopzee228@icloud.com>
2024-04-29 13:40:39 +0300

    add JavaDoc to CreditController

--- MTSFinalProject/src/main/java/com/example/mtsfinalproject/controller/CreditController.java
+++ MTSFinalProject/src/main/java/com/example/mtsfinalproject/controller/CreditController.java
@@ -11,40 +11,68 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 
-/*
-* Класс-контроллер предоставляет эндпоинты для взаимодейсивия с сервисом кредитования
-*
-* */
+/**
+ * Класс-контроллер, предоставляет эндпоинты для взаимодейсивия с сервисом кредитования
+ */
 @RestController
 @RequiredArgsConstructor
 public class CreditController {
 
     private final CreditService creditService;
 
+    /**
+     * Метод для получения тарифов на кредит
+     *
+     * @return Обертка с информацией о тарифах
+     */
     @GetMapping("getTariffs")
     public ResponseEntity<DataWrap> getTariffs() {
         return ResponseEntity.ok(new DataWrap(new TariffsWrap(creditService.getTariffs())));
     }
 
+    /**
+     * Метод для получения заявок пользователя
+     *
+     * @param userId Идентификатор пользователя
+     * @return Обертка с информацией о заявках пользователя
+     */
     @PostMapping("getUserOrders")
-    public ResponseEntity<DataWrap> getUserOrders(@RequestBody Long userId){
+    public ResponseEntity<DataWrap> getUserOrders(@RequestBody Long userId) {
         return ResponseEntity.ok(new DataWrap(new LoanOrdersWrap(creditService.getUserOrders(userId))));
     }
 
+    /**
+     * Метод для создания новой заявки на кредит
+     *
+     * @param dto Информация о создаваемой заявке
+     * @return Обертка с идентификатором созданной заявки
+     */
     @PostMapping("order")
     public ResponseEntity<DataWrap> createOrder(@RequestBody LoanOrderCreateDto dto) {
         return ResponseEntity.ok(new DataWrap(new OrderIdWrap(creditService.createOrder(dto).getOrderId())));
     }
 
+    /**
+     * Метод для получения статуса заявки
+     *
+     * @param orderId Идентификатор заявки
+     * @return Обертка с информацией о статусе заявки
+     */
     @GetMapping("getStatusOrder")
     public ResponseEntity<DataWrap> getStatusOrder(@RequestParam("orderId") UUID orderId) {
         return ResponseEntity.ok(new DataWrap(new OrderStatusWrap(creditService.getStatusOrder(orderId).getStatus())));
     }
 
+    /**
+     * Метод для удаления заявки на кредит
+     *
+     * @param dto Информация о заявке, которую необходимо удалить
+     * @return Ответ об успешном удалении заявки
+     */
     @DeleteMapping("deleteOrder")
     public ResponseEntity<Void> deleteOrder(@RequestBody LoanOrderDeleteDto dto) {
-            creditService.deleteOrder(dto);
-            return ResponseEntity.ok().build();
+        creditService.deleteOrder(dto);
+        return ResponseEntity.ok().build();
     }
 }
 
